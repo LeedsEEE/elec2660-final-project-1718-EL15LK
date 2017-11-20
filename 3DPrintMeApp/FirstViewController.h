7e95eab3a1145a3c49562028e8352962685ec43e
Lakshit Kumar [el15lk] <el15lk@eentpc247.leeds.ac.uk>
2017-11-20 16:10:47 +0000

    First tab now has all the labels, text fields and a switch to control the type of input. The switch is also programmed to hide and show the elements as appropriate.

--- 3DPrintMeApp/FirstViewController.h
+++ 3DPrintMeApp/FirstViewController.h
@@ -10,6 +10,18 @@
 
 @interface FirstViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *lengthLabel;                 //Labels and Text Fields that will be used for dimensions input
+@property (weak, nonatomic) IBOutlet UILabel *widthLabel;
+@property (weak, nonatomic) IBOutlet UILabel *heightLabel;
+@property (weak, nonatomic) IBOutlet UITextField *lengthTextField;
+@property (weak, nonatomic) IBOutlet UITextField *widthTextField;
+@property (weak, nonatomic) IBOutlet UITextField *heightTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *volumeTextField;         //Label and Text Field that will be used for volume input
+@property (weak, nonatomic) IBOutlet UILabel *volumeLabel;
+
+
+- (IBAction)switchPressed:(UISwitch *)sender;                              //Switch used to select the type of input
 
 @end
 
--- 3DPrintMeApp/FirstViewController.m
+++ 3DPrintMeApp/FirstViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.volumeLabel.hidden = true;                                //Switch is turned off by default (dimensions input)
+    self.volumeTextField.hidden = true;                            //So hide all elements that belong to volume input
+    
 }
 
 
@@ -26,4 +30,31 @@
 }
 
 
+- (IBAction)switchPressed:(UISwitch *)sender {
+    
+    if (sender.on)  {                                              // IF switch is turned on (volume is selected)
+        
+        self.lengthLabel.hidden = true;                            // Hide all elements that belong to dimensions input
+        self.widthLabel.hidden = true;
+        self.heightLabel.hidden = true;
+        self.lengthTextField.hidden = true;
+        self.widthTextField.hidden = true;
+        self.heightTextField.hidden = true;
+        
+        self.volumeLabel.hidden = false;                           // Show all elements that belong to volume input
+        self.volumeTextField.hidden = false;
+        
+    }   else {                                                     // Else switch is turned off (dimensions is selected)
+        
+        self.volumeLabel.hidden = true;                            // Hide all elements that belong to volume input
+        self.volumeTextField.hidden = true;
+        
+        self.lengthLabel.hidden = false;                           // Show all elements that belong to dimensions input
+        self.widthLabel.hidden = false;
+        self.heightLabel.hidden = false;
+        self.lengthTextField.hidden = false;
+        self.widthTextField.hidden = false;
+        self.heightTextField.hidden = false;
+    }
+}
 @end
